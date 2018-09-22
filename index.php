c17ba15981edf0d0517bb3e64c72c7da2b156ec5
hashmoker <hashmoker@gmail.com>
2018-09-22 20:43:27 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//Коментарий
+
+/*
+/*
+ * *
+ */
+$var = 1;
+$var1 = 2;
+echo $var . $var1; //12
+?>
+
+<br>
+<?php
+echo $var + $var1;//3
+
+echo '<a href="http://vk.com">1111</a>';
+?>
