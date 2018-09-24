73ef662e752b134085639b78fdab3d19a70485a9
hashmoker <hashmoker@gmail.com>
2018-09-24 19:51:14 +0300

    add lesson2

--- 2.php
+++ 2.php
@@ -1,24 +1,4 @@
 <?php
-
-
-//Коментарий
-
-/*
-/*
- * *
- */
-$var = 1;
-$var1 = 2;
-echo $var . $var1; //12
-?>
-
-<br>
-<?php
-echo $var + $var1;//3
-
-echo '<a href="http://vk.com">1111</a>';
-
-
 //echo $str = "Hello";
 //$arr = ["orange", "red", "white"];
 //echo $arr [0];// orange
@@ -45,4 +25,3 @@ if (isset($var)){
 }else{
     echo"not";
 }
-?>
