fc3722228264adfd45ed4c2715dd6fdaea630ce3
hashmoker <hashmoker@gmail.com>
2018-09-24 21:13:27 +0300

    333

--- 2.php
+++ 2.php
@@ -29,22 +29,49 @@
 //
 //echo "<pre>";
 //print_r($assoc);
+//
+//$number1=102;
+//$number2=543;
+//$number3=12;
+//echo $number1+$number2;
+//echo"<br>";
+//echo $number2-$number3;
+//echo"<br>";
+//echo $number2/$number3;
+//echo"<br>";
+//echo $number2*$number3;
+//echo"<br>";
+//echo $number2%$number3;
+//echo"<br>";
+//if (($number1 > 26 && $number1 < $number2) or ($number1 = 25 and $number3 == 100 and $number2 == 70))
+//{echo "ok";}
 
-$number1=102;
-$number2=543;
-$number3=12;
-echo $number1+$number2;
-echo"<br>";
-echo $number2-$number3;
-echo"<br>";
-echo $number2/$number3;
-echo"<br>";
-echo $number2*$number3;
-echo"<br>";
-echo $number2%$number3;
-echo"<br>";
-if (($number1 > 26 && $number1 < $number2) or ($number1 = 25 and $number3 == 100 and $number2 == 70))
-{echo "ok";}
+
+
+//for();
+//while();
+//do while();
+
+
+
+
+$assoc = [
+    
+    'color' => 'red',
+    'age' => 25,
+    'nick' => 'Max'
+    
+    
+];
+
+foreach($assoc as $item)
+{
+    echo $item. "<br>";   
+}
+    
+    
+    
+    
 
 
 
