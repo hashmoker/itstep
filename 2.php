5054a7fa9e36b792201f84292142bd1e2a06cac5
hashmoker <hashmoker@gmail.com>
2018-09-24 20:59:52 +0300

    222

--- 2.php
+++ 2.php
@@ -1,31 +1,66 @@
 <?php
-//echo $str = "Hello";
-//$arr = ["orange", "red", "white"];
-//echo $arr [0];// orange
+////echo $str = "Hello";
+////$arr = ["orange", "red", "white"];
+////echo $arr [0];// orange
+////
+////
+//$assoc = [
+//
+//   'color' => 'red',
+//   'age' => 25,
+//   'nick' => 'Max'
+//];
+////echo $assoc ['color'];//red
+////$obj ->color;//red
 //
 //
-$assoc = [
+////$var = [];
+////
+//////unset($var);
+////
+////if (isset($var)){
+////
+////    echo"ok";
+////
+////}else{
+////    echo"not";
+////}
+//
+//
+//echo "<pre>";
+//print_r($assoc);
 
-   'color' => 'red',
-   'age' => 25,
-   'nick' => 'Max'
-];
-//echo $assoc ['color'];//red
-//$obj ->color;//red
+$number1=102;
+$number2=543;
+$number3=12;
+echo $number1+$number2;
+echo"<br>";
+echo $number2-$number3;
+echo"<br>";
+echo $number2/$number3;
+echo"<br>";
+echo $number2*$number3;
+echo"<br>";
+echo $number2%$number3;
+echo"<br>";
+if (($number1 > 26 && $number1 < $number2) or ($number1 = 25 and $number3 == 100 and $number2 == 70))
+{echo "ok";}
 
 
-//$var = [];
-//
-////unset($var);
-//
-//if (isset($var)){
-//
-//    echo"ok";
-//
-//}else{
-//    echo"not";
-//}
 
 
-echo "<pre>";
-print_r($assoc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
