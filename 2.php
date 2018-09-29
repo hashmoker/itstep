811a2f4ee99849269a13296fcbd9d4f1580e8e32
hashmoker <hashmoker@gmail.com>
2018-09-29 19:03:37 +0300

    4444

--- 2.php
+++ 2.php
@@ -1,84 +1,30 @@
-<?php
-////echo $str = "Hello";
-////$arr = ["orange", "red", "white"];
-////echo $arr [0];// orange
-////
-////
-//$assoc = [
-//
-//   'color' => 'red',
-//   'age' => 25,
-//   'nick' => 'Max'
-//];
-////echo $assoc ['color'];//red
-////$obj ->color;//red
-//
-//
-////$var = [];
-////
-//////unset($var);
-////
-////if (isset($var)){
-////
-////    echo"ok";
-////
-////}else{
-////    echo"not";
-////}
-//
-//
-//echo "<pre>";
-//print_r($assoc);
-//
-//$number1=102;
-//$number2=543;
-//$number3=12;
-//echo $number1+$number2;
-//echo"<br>";
-//echo $number2-$number3;
-//echo"<br>";
-//echo $number2/$number3;
-//echo"<br>";
-//echo $number2*$number3;
-//echo"<br>";
-//echo $number2%$number3;
-//echo"<br>";
-//if (($number1 > 26 && $number1 < $number2) or ($number1 = 25 and $number3 == 100 and $number2 == 70))
-//{echo "ok";}
+<!doctype html>
+
+<body>
 
 
+<ul class="nav">
 
-//for();
-//while();
-//do while();
-
-
-
-
-$assoc = [
-    
-    'color' => 'red',
-    'age' => 25,
-    'nick' => 'Max'
-    
-    
-];
-
-foreach($assoc as $item)
-{
-    echo $item. "<br>";   
-}
-    
+    <?php
+    $nav = 4;
+    $cur_page = 1;
+    while ($cur_page <= $nav){
+        //первая итерация $cur_page = 1
+        echo"<li class=\"nav-item\">
+             <a class=\"nav-link active\" href=\"?page=$cur_page\">LINK</a>
+             </li>";
+        $cur_page++;
+        
+    }
     
     
     
 
+    ?>
 
-
-
-
-
-
+</ul>
+</body>
+</html>
 
 
 
