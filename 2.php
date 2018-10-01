e98419ac2c4167348f084d0caaf8c571052e10b3
hashmoker <hashmoker@gmail.com>
2018-10-01 20:55:09 +0300

    555

--- 2.php
+++ 2.php
@@ -1,36 +1,97 @@
-<!doctype html>
+<?php
 
-<body>
+//function calculate($number1, $number2) {
+//    $sum = $number1 + $number2;
+//    return $sum;
+//}
+//
+//echo calculate(1,3); // 4
+//
+//
+//
+
+    // внешние переменные
+//    $first = 4;
+//    $second = 6;
+//    // аргументы, как способ передать внешнюю переменную внутрь
+//    function calculate($number1, $number2) {
+//        // Переменная отсюда не видна
+//        echo $first;
+//
+//        echo $number1;
+//    }
+//
+//    echo calculate($first, $second);
+
+//    $fruits = array("lemon", "orange", "banana", "apple");
+//
+//    array_walk($fruits, function($value) {
+//        echo $value . ", ";
+//    });
+//    // array_walk - встроенная функция
+//    // $fruits - первый аргумент, массив
+//    // function($value) - второй аргумент, анонимная функция
 
 
-<ul class="nav">
 
-    <?php
-    $nav = 4;
-    $cur_page = 1;
-    while ($cur_page <= $nav){
-        //первая итерация $cur_page = 1
-        echo"<li class=\"nav-item\">
-             <a class=\"nav-link active\" href=\"?page=$cur_page\">LINK</a>
-             </li>";
-        $cur_page++;
-        
-    }
-    
-    
-    
 
-    ?>
-
-</ul>
-</body>
-</html>
+//
+//    $title = "Lorem Ipsum is simply dummy text";
+//
+//    function cut_text($text, $num_letters) {
+//        $num = strlen($text);
+//
+//        if ($num > $num_letters) {
+//            $text = substr($text, 0, $num_letters);
+//            $text .= "...";
+//        }
+//
+//        return $text;
+//    }
+//
 
 
 
 
 
+//function calculate($number1, $number2) {
+//    $sum = $number1 + $number2;
+//    return $sum;
+//}
+//
+//echo calculate(1,3);
+//
+//
 
+//
+function sayHello($name){
+    echo "Привет, $name!<br/>";
+}
+sayHello("Вася");
+
+
+//function pifagor($cols, $rows)
+//    {
+//    $tr=1;
+//    echo "<table border='1'>" ;
+//    while($tr<=$rows)
+//    {
+//    echo "<tr>" ;
+//    $td=1;
+//    while ($td<=$cols)
+//    {
+//        echo "<td>".$tr*$td."</td>";
+//        $td++;
+//    }
+//    echo "</tr>";
+//    $tr++ ;
+//    }
+//    echo "</table>" ;
+//    }
+//
+//    pifagor(9,9)
+
+?>
 
 
 
